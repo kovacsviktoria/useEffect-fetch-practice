055df83389f73884079378737f47360a2bb4912c
Viktoria Toth <k.viki1117@gmail.com>
2021-08-30 18:50:48 +0200

    added async fetch

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    (async ()=>{
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await response.json();
+      setData(data.splice(0, 10));
+    })();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {data.map((item) => {
+          return <li key={item.id}>{item.body}</li>
+        })}
+      </ul>
+    </div>
+  )
+;
 }
 
 export default App;
+
+
+//useEffect with .then
+// useEffect(() => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((data) => setData(data.splice(0, 10)));
+// }, []);
